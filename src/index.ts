305609a04153e48040c3bd8cc8c58ae667ee8280
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2023-04-22 19:08:10 -0300

    feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hola")
+import express, { urlencoded } from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+const port = process.env.PORT || 3333
+
+const server = express()
+dotenv.config()
+server.use(urlencoded({ extended: true }))
+server.use(cors)
+server.listen(port, () => {
+  console.log("Running at port", port)
+})
