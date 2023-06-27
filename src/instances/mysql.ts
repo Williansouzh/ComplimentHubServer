560cecc5ec7c0ae63b05616816f8368e3333664c
Willian <williansouza4041@gmail.com>
2023-06-26 22:03:39 -0300

    fix: add external url database

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -3,15 +3,10 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE as string,
-  process.env.MYSQL_USER as string,
-  process.env.MYSQL_PASSWORD as string,
-  {
-    port: parseInt(process.env.MYSQL_PORT as string),
-    dialect: "mysql",
-  }
-)
+export const sequelize = new Sequelize(process.env.MYSQL_DATABASE as string, {
+  dialect: "mysql",
+  port: parseInt(process.env.MYSQL_PORT as string),
+})
 
 sequelize
   .authenticate()
